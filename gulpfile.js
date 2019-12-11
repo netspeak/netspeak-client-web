0efa37e147eb97048b3e22253a001914e76bc4c2
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-12-11 22:11:48 +0100

    New publish scripts

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const git = require("simple-git/promise");
+const sh = require("shelljs");
+
+
+async function publishDemo() {
+	sh.cd(__dirname);
+
+	sh.rm("-rf", "publish");
+	sh.exec("git clone https://github.com/netspeak/netspeak.github.io.git publish");
+	const repo = git(__dirname + "/publish");
+	await repo.status(); // verify repo
+
+	sh.exec("npm run build");
+
+	sh.rm("-rf", "publish/demo");
+	sh.mkdir("-p", "publish/demo");
+	sh.cp("-r", "./build/default/*", "./publish/demo");
+
+	const status = await repo.status();
+
+	if (status.files.length === 0) {
+		console.log("Nothing changed. No commit will be made.");
+		return;
+	}
+
+	await repo.add("*");
+	await repo.commit("Published demo");
+	await repo.push();
+}
+
+async function publishRelease() {
+	sh.cd(__dirname);
+
+	sh.rm("-rf", "publish");
+	sh.exec("git clone https://github.com/netspeak/netspeak.github.io.git publish");
+	const repo = git(__dirname + "/publish");
+	await repo.status(); // verify repo
+
+	sh.exec("npm run build");
+
+	sh.cd("./publish");
+	sh.ls().forEach(file => {
+		if (!/^(?:\.git|\.nojekyll|demo|CNAME)$/i.test(file)) {
+			sh.rm("-rf", file);
+		}
+	});
+	sh.cd("..");
+	sh.cp("-r", "./build/default/*", "./publish");
+
+	const status = await repo.status();
+
+	if (status.files.length === 0) {
+		console.log("Nothing changed. No commit will be made.");
+		return;
+	}
+
+	await repo.add("*");
+	await repo.commit("Published release");
+	await repo.push();
+}
+
+
+module.exports = {
+	publishDemo,
+	publishRelease
+};
